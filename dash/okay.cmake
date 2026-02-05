c6c63829f9db31029fcc479b641d484083f5c3b2
evan-bertis-sample <esample21@gmail.com>
2026-02-05 16:00:13 -0600

    Begin working on cmake, add can lib as submodule

--- dash/okay.cmake
+++ dash/okay.cmake
@@ -1,3 +1,6 @@
+# okay.cmake -- links to okay engine
+
+# dash application
 set(SOURCES
     ${OKAY_PROJECT_ROOT_DIR}/main.cpp
 )
@@ -5,6 +8,14 @@ set(SOURCES
 set(INCLUDES
     ${OKAY_PROJECT_ROOT_DIR}
 )
+
+# add needed drivers
+# for now only CAN
+set(DRIVERS_DIR ${OKAY_PROJECT_ROOT_DIR}/drivers)
+add_subdirectory(${DRIVERS_DIR}/can) # produces a static library nfr_canlib
+
+# add platform-specific functionalities
+add_subdirectory(${OKAY_PROJECT_ROOT_DIR}/platform)
 
 # add the sources and includes to PROJECT executable
 target_sources(${PROJECT} PRIVATE ${SOURCES})
---
+++ dash/platform/platform.hpp
@@ -0,0 +1,9 @@
+#ifndef __PLATFORM_H__
+#define __PLATFORM_H__
+
+namespace dash::platform {
+
+
+};
+
+#endif // __PLATFORM_H__

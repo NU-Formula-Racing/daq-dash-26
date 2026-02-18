aa22409f01eafab2b7b091c32d70e96f3347f8c0
kodeti <kiritortino@gmail.com>
2026-02-17 21:29:21 -0600

    mirrored mcp2515 in can_imgui (temp)

--- dash/can/mock/can_imgui.hpp
+++ dash/can/mock/can_imgui.hpp
@@ -5,8 +5,16 @@
 #include <nfr_can/CAN_interface.hpp>
 
 class CAN_IMGUI : public ICAN {
-    ICAN() = default;
-    
+private:
+    ISpi& _spi;
+    IGpio& _cs;
+    IClock& _clock;
+    uint32_t missCounter;
+    uint32_t recvCount;
+
+public:
+    CAN_IMGUI(ISpi& spi, IGpio& cs, IClock& clock) : _spi(spi), _cs(cs), _clock(clock) {}
+
     bool init(const BaudRate baud) override;
     bool send(const CAN_Frame& msg) override;
     bool recv(CAN_Frame& msg) override;

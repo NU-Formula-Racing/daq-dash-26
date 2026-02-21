68503bd18169241493906e0a4eb2e3d897baa68b
AwesomeDudeDK <jeyeon.kim@gmail.com>
2026-02-21 14:04:14 -0600

    generate ui for mock signal fields
    Co-authored-by: Jalil Hemphill <jalilhemphill1@users.noreply.github.com>
    Co-authored-by: kodeti <kodeti@users.noreply.github.com>

--- dash/can/mock/can_imgui.cpp
+++ dash/can/mock/can_imgui.cpp
@@ -1,9 +1,64 @@
 #include "can_imgui.hpp"
+#include "can/can_dbc.hpp"
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include "nfr_can/CAN_interface.hpp"
+#include <unordered_map>
 
 bool CAN_IMGUI::init(const BaudRate baud) {
+    for (ICAN_Message* message : dbc::driveBus.get_messages()) {
+        uint32_t messageId { message->get_id().id };
+        for (uint8_t sigNum {}; sigNum < message->get_num_signals(); sigNum++) {
+            auto sigId { std::pair{message->get_id().id, sigNum} };
+
+            char* name;
+            auto it { dbc::meta::signalIdToName.find(sigId) };
+            if (it != dbc::meta::signalIdToName.end()) {
+                name = const_cast<char*>(it->second);
+            }
+            
+            ICAN_Signal* signal { message->get_signal(sigNum) };
+
+            struct SignalInfo sigInfo;
+            sigInfo.name = name;
+
+            messageIdToSignalsInfo[messageId].push_back(sigInfo);
+            
+            switch (signal->getSignalType()) {
+                case SignalType::INT8:
+                    sigInfo.value.s8 = 0;
+                    break;
+                case SignalType::INT16:
+                    sigInfo.value.s16 = 0;
+                    break;
+                case SignalType::INT32:
+                    sigInfo.value.s32 = 0;
+                    break;
+                case SignalType::INT64:
+                    sigInfo.value.s64 = 0;
+                    break;
+                case SignalType::UINT8:
+                    sigInfo.value.u8 = 0;
+                    break;
+                case SignalType::UINT16:
+                    sigInfo.value.u16 = 0;
+                    break;
+                case SignalType::UINT32:
+                    sigInfo.value.u32 = 0;
+                    break;
+                case SignalType::UINT64:
+                    sigInfo.value.u64 = 0;
+                    break;
+                case SignalType::FLOAT:
+                    sigInfo.value.f = 0;
+                    break;
+                case SignalType::BOOL:
+                    sigInfo.value.b = false;
+                    break;
+            }
+        }
+    }
     return true;
 } // do nothing
 
@@ -17,14 +72,55 @@ bool CAN_IMGUI::recv(CAN_Frame& msg) {
     ImGui::NewFrame();
     
     ImGui::Begin("CAN");
-    if (ImGui::CollapsingHeader("PDMInfo", ImGuiTreeNodeFlags_DefaultOpen)) {
-        ImGui::InputFloat("batVoltage", &pdmInfo.batVoltage);
-        ImGui::InputFloat("batCurr", &pdmInfo.batCur);
-    }
-    if (ImGui::CollapsingHeader("bms_temp0", ImGuiTreeNodeFlags_DefaultOpen)) {
-        // ImGui::InputFloat("cell0", &);
-    }
+    
+    for (ICAN_Message* message : dbc::driveBus.get_messages()) {
+        uint32_t messageId = message->get_id().id;
 
+        if (msg._id == messageId) {
+            message->decode_from(msg);
+        }
+
+        if (ImGui::CollapsingHeader(dbc::meta::messageIdToName.at(messageId), ImGuiTreeNodeFlags_DefaultOpen)) {
+            for (uint8_t sigNum {}; sigNum < message->get_num_signals(); sigNum++) {
+                auto sigId { std::pair{message->get_id().id, sigNum} };
+                
+                ICAN_Signal* signal { message->get_signal(sigNum) };
+                
+                switch (signal->getSignalType()) {
+                    case SignalType::INT8:
+                        ImGui::InputScalar(messageIdToSignalsInfo[messageId][sigNum].name, ImGuiDataType_S8, &messageIdToSignalsInfo[messageId][sigNum].value.s8);
+                        break;
+                    case SignalType::INT16:
+                        ImGui::InputScalar(messageIdToSignalsInfo[messageId][sigNum].name, ImGuiDataType_S16, &messageIdToSignalsInfo[messageId][sigNum].value.s16);
+                        break;
+                    case SignalType::INT32:
+                        ImGui::InputScalar(messageIdToSignalsInfo[messageId][sigNum].name, ImGuiDataType_S32, &messageIdToSignalsInfo[messageId][sigNum].value.s32);
+                        break;
+                    case SignalType::INT64:
+                        ImGui::InputScalar(messageIdToSignalsInfo[messageId][sigNum].name, ImGuiDataType_S64, &messageIdToSignalsInfo[messageId][sigNum].value.s64);
+                        break;
+                    case SignalType::UINT8:
+                        ImGui::InputScalar(messageIdToSignalsInfo[messageId][sigNum].name, ImGuiDataType_U8, &messageIdToSignalsInfo[messageId][sigNum].value.u8);
+                        break;
+                    case SignalType::UINT16:
+                        ImGui::InputScalar(messageIdToSignalsInfo[messageId][sigNum].name, ImGuiDataType_U16, &messageIdToSignalsInfo[messageId][sigNum].value.u16);
+                        break;
+                    case SignalType::UINT32:
+                        ImGui::InputScalar(messageIdToSignalsInfo[messageId][sigNum].name, ImGuiDataType_U32, &messageIdToSignalsInfo[messageId][sigNum].value.u32);
+                        break;
+                    case SignalType::UINT64:
+                        ImGui::InputScalar(messageIdToSignalsInfo[messageId][sigNum].name, ImGuiDataType_U64, &messageIdToSignalsInfo[messageId][sigNum].value.u64);
+                        break;
+                    case SignalType::FLOAT:
+                        ImGui::InputFloat(messageIdToSignalsInfo[messageId][sigNum].name, &messageIdToSignalsInfo[messageId][sigNum].value.f);
+                        break;
+                    case SignalType::BOOL:
+                        ImGui::Checkbox(messageIdToSignalsInfo[messageId][sigNum].name, &messageIdToSignalsInfo[messageId][sigNum].value.b);
+                        break;
+                }
+            }
+        }
+    }
     ImGui::End();
 
     ImGui::Render();
@@ -38,4 +134,6 @@ bool CAN_IMGUI::recv(CAN_Frame& msg) {
 uint32_t CAN_IMGUI::time_ms() {
     return 0;
 } // do nothing
+
+
 
--- dash/can/mock/can_imgui.hpp
+++ dash/can/mock/can_imgui.hpp
@@ -2,16 +2,30 @@
 #define __CAN_IMGUI_H__
 
 #include <cstdint>
+#include <unordered_map>
+#include <vector>
 #include <nfr_can/CAN_interface.hpp>
 
 class CAN_IMGUI : public ICAN {
 private:
-    struct PDMInfo {
-        float batVoltage;
-        float batCur;
+    struct SignalInfo {
+        char* name;
+
+        union {
+            int8_t s8;
+            int16_t s16;
+            int32_t s32;
+            int64_t s64;
+            uint8_t u8;
+            uint16_t u16;
+            uint32_t u32;
+            uint64_t u64;
+            float f;
+            bool b;
+        } value;
     };
 
-    PDMInfo pdmInfo {};
+    std::unordered_map<uint32_t, std::vector<SignalInfo>> messageIdToSignalsInfo;
 
 public:
     CAN_IMGUI() {}

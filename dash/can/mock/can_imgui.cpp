1d57cb91bf7d71fe3b7486616a59d6a8b20a1637
AwesomeDudeDK <jeyeon.kim@gmail.com>
2026-03-02 00:41:01 -0600

    refactor can_imgui so longer uses unnecessary map

--- dash/can/mock/can_imgui.cpp
+++ dash/can/mock/can_imgui.cpp
@@ -3,85 +3,25 @@
 #include "can/can_dbc.hpp"
 #include "imgui.h"
 #include "nfr_can/CAN_interface.hpp"
-#include "okay/core/util/option.hpp"
 #include <cstdint>
 #include <cstdlib>
-#include <unordered_map>
 
 bool CAN_IMGUI::init(const BaudRate baud) {
-    for (ICAN_Message* message : dbc::driveBus.get_messages()) {
-        uint32_t messageId { message->get_id().id };
-        for (uint8_t sigNum {}; sigNum < message->get_num_signals(); sigNum++) {
-            auto sigId { std::pair{message->get_id().id, sigNum} };
-
-            char* name;
-            auto it { dbc::meta::signalIdToName.find(sigId) };
-            if (it != dbc::meta::signalIdToName.end()) {
-                name = const_cast<char*>(it->second);
-            }
-            
-            ICAN_Signal* signal { message->get_signal(sigNum) };
-
-            struct SignalInfo sigInfo;
-            sigInfo.name = name;
-            sigInfo.type = signal->getSignalType();
-
-            switch (sigInfo.type) {
-                case SignalType::INT8:    sigInfo.value.s8 = 0;     break;
-                case SignalType::INT16:   sigInfo.value.s16 = 0;    break;
-                case SignalType::INT32:   sigInfo.value.s32 = 0;    break;
-                case SignalType::INT64:   sigInfo.value.s64 = 0;    break;
-                case SignalType::UINT8:   sigInfo.value.u8 = 0;     break;
-                case SignalType::UINT16:  sigInfo.value.u16 = 0;    break;
-                case SignalType::UINT32:  sigInfo.value.u32 = 0;    break;
-                case SignalType::UINT64:  sigInfo.value.u64 = 0;    break;
-                case SignalType::FLOAT:   sigInfo.value.f = 0;      break;
-                case SignalType::BOOL:    sigInfo.value.b = false;  break;
-            }
-
-            messageIdToSignalsInfo[messageId].push_back(sigInfo);
-        }
-    }
     return true;
-}
+} // do nothing
 
 bool CAN_IMGUI::send(const CAN_Frame& msg) {
     return true;
 } // do nothing
 
 bool CAN_IMGUI::recv(CAN_Frame& msg) {
-    okay::Option<MessageChangeInfo> changeInfoOpt { drawUI() };
-    if (changeInfoOpt.isNone()) return false;
-
-    MessageChangeInfo info { changeInfoOpt.value() };
-
-    ICAN_Message *message { dbc::driveBus.get_message_from_id(info.messageID) };
-
-    ICAN_Signal *signal { message->get_signal(info.signalNum) };
-
-    // we need to set the signal to the value in info.changedSigna
-    switch (info.changedSignal.type) {
-        case SignalType::INT8:    signal->set(info.changedSignal.type, &info.changedSignal.value.s8);   break;
-        case SignalType::INT16:   signal->set(info.changedSignal.type, &info.changedSignal.value.s16);  break;
-        case SignalType::INT32:   signal->set(info.changedSignal.type, &info.changedSignal.value.s32);  break;
-        case SignalType::INT64:   signal->set(info.changedSignal.type, &info.changedSignal.value.s64);  break;
-        case SignalType::UINT8:   signal->set(info.changedSignal.type, &info.changedSignal.value.u8);   break;
-        case SignalType::UINT16:  signal->set(info.changedSignal.type, &info.changedSignal.value.u16);  break;
-        case SignalType::UINT32:  signal->set(info.changedSignal.type, &info.changedSignal.value.u32);  break;
-        case SignalType::UINT64:  signal->set(info.changedSignal.type, &info.changedSignal.value.u64);  break;
-        case SignalType::FLOAT:   signal->set(info.changedSignal.type, &info.changedSignal.value.f);    break;
-        case SignalType::BOOL:    signal->set(info.changedSignal.type, &info.changedSignal.value.b);    break;
-    }
-    
-    message->encode_to_frame(msg);
+    drawUI();
     return false;
 }
 
-okay::Option<CAN_IMGUI::MessageChangeInfo> CAN_IMGUI::drawUI() {
+void CAN_IMGUI::drawUI() {
     ImGui::Begin("CAN");
 
-    auto result { okay::Option<CAN_IMGUI::MessageChangeInfo>::none() };
-    
     for (ICAN_Message* message : dbc::driveBus.get_messages()) {
         uint32_t messageId { message->get_id().id };
 
@@ -89,42 +29,32 @@ okay::Option<CAN_IMGUI::MessageChangeInfo> CAN_IMGUI::drawUI() {
             for (uint8_t sigNum {}; sigNum < message->get_num_signals(); sigNum++) {
                 ICAN_Signal* signal { message->get_signal(sigNum) };
 
-                SignalInfo& sigInfo { messageIdToSignalsInfo[messageId][sigNum] };
-                
-                auto logOnEdit { [&](const auto& val) {
-                    // if (ImGui::IsItemDeactivatedAfterEdit()) {
-                        // okay::Engine.logger.debug("Value for {} changed to: {}", sigInfo.name, val);
-                        // std::cout << std::flush;
-                    // }
-                    result = okay::Option<CAN_IMGUI::MessageChangeInfo>::some({ messageId, sigNum, sigInfo });
-                }};
-                
-                std::string name { std::string(sigInfo.name) + "###" + dbc::meta::messageIdToName.at(messageId) + sigInfo.name };
-                
-                switch (sigInfo.type) {
-                    case SignalType::INT8:    ImGui::InputScalar(name.c_str(), ImGuiDataType_S8, &sigInfo.value.s8);    logOnEdit(sigInfo.value.s8);   break;
-                    case SignalType::INT16:   ImGui::InputScalar(name.c_str(), ImGuiDataType_S16, &sigInfo.value.s16);  logOnEdit(sigInfo.value.s16);  break;
-                    case SignalType::INT32:   ImGui::InputScalar(name.c_str(), ImGuiDataType_S32, &sigInfo.value.s32);  logOnEdit(sigInfo.value.s32);  break;
-                    case SignalType::INT64:   ImGui::InputScalar(name.c_str(), ImGuiDataType_S64, &sigInfo.value.s64);  logOnEdit(sigInfo.value.s64);  break;
-                    case SignalType::UINT8:   ImGui::InputScalar(name.c_str(), ImGuiDataType_U8, &sigInfo.value.u8);    logOnEdit(sigInfo.value.u8);   break;
-                    case SignalType::UINT16:  ImGui::InputScalar(name.c_str(), ImGuiDataType_U16, &sigInfo.value.u16);  logOnEdit(sigInfo.value.u16);  break;
-                    case SignalType::UINT32:  ImGui::InputScalar(name.c_str(), ImGuiDataType_U32, &sigInfo.value.u32);  logOnEdit(sigInfo.value.u32);  break;
-                    case SignalType::UINT64:  ImGui::InputScalar(name.c_str(), ImGuiDataType_U64, &sigInfo.value.u64);  logOnEdit(sigInfo.value.u64);  break;
-                    case SignalType::FLOAT:   ImGui::InputFloat(name.c_str(), &sigInfo.value.f);                        logOnEdit(sigInfo.value.f);    break;
-                    case SignalType::BOOL:    ImGui::Checkbox(name.c_str(), &sigInfo.value.b);                          logOnEdit(sigInfo.value.b);    break;
+                auto sigId { std::pair{messageId, sigNum} };
+                const char* name = "(unknown)";
+                auto it { dbc::meta::signalIdToName.find(sigId) };
+                if (it != dbc::meta::signalIdToName.end()) name = it->second;
+
+                std::string widgetName { std::string(name) + "###" + dbc::meta::messageIdToName.at(messageId) + name };
+
+                switch (signal->getSignalType()) {
+                    case SignalType::INT8:   { auto s = static_cast<CAN_Signal<int8_t>*>(signal);   int8_t   v = s->get(); if (ImGui::InputScalar(widgetName.c_str(), ImGuiDataType_S8,  &v)) s->set(v); break; }
+                    case SignalType::INT16:  { auto s = static_cast<CAN_Signal<int16_t>*>(signal);  int16_t  v = s->get(); if (ImGui::InputScalar(widgetName.c_str(), ImGuiDataType_S16, &v)) s->set(v); break; }
+                    case SignalType::INT32:  { auto s = static_cast<CAN_Signal<int32_t>*>(signal);  int32_t  v = s->get(); if (ImGui::InputScalar(widgetName.c_str(), ImGuiDataType_S32, &v)) s->set(v); break; }
+                    case SignalType::INT64:  { auto s = static_cast<CAN_Signal<int64_t>*>(signal);  int64_t  v = s->get(); if (ImGui::InputScalar(widgetName.c_str(), ImGuiDataType_S64, &v)) s->set(v); break; }
+                    case SignalType::UINT8:  { auto s = static_cast<CAN_Signal<uint8_t>*>(signal);  uint8_t  v = s->get(); if (ImGui::InputScalar(widgetName.c_str(), ImGuiDataType_U8,  &v)) s->set(v); break; }
+                    case SignalType::UINT16: { auto s = static_cast<CAN_Signal<uint16_t>*>(signal); uint16_t v = s->get(); if (ImGui::InputScalar(widgetName.c_str(), ImGuiDataType_U16, &v)) s->set(v); break; }
+                    case SignalType::UINT32: { auto s = static_cast<CAN_Signal<uint32_t>*>(signal); uint32_t v = s->get(); if (ImGui::InputScalar(widgetName.c_str(), ImGuiDataType_U32, &v)) s->set(v); break; }
+                    case SignalType::UINT64: { auto s = static_cast<CAN_Signal<uint64_t>*>(signal); uint64_t v = s->get(); if (ImGui::InputScalar(widgetName.c_str(), ImGuiDataType_U64, &v)) s->set(v); break; }
+                    case SignalType::FLOAT:  { auto s = static_cast<CAN_Signal<float>*>(signal);    float    v = s->get(); if (ImGui::InputFloat(widgetName.c_str(),                    &v)) s->set(v); break; }
+                    case SignalType::BOOL:   { auto s = static_cast<CAN_Signal<bool>*>(signal);     bool     v = s->get(); if (ImGui::Checkbox(widgetName.c_str(),                       &v)) s->set(v); break; }
                 }
             }
         }
     }
+
     ImGui::End();
-    
-    return result;
 }
 
 uint32_t CAN_IMGUI::time_ms() {
     return 0;
 } // do nothing
-
-CAN_IMGUI::SignalInfo* CAN_IMGUI::getSignalInfo(uint32_t msgId, uint8_t sigNum) {
-    return &messageIdToSignalsInfo.at(msgId).at(sigNum);
-}
--- dash/can/mock/can_imgui.hpp
+++ dash/can/mock/can_imgui.hpp
@@ -2,37 +2,9 @@
 #define __CAN_IMGUI_H__
 
 #include <cstdint>
-#include <unordered_map>
-#include <vector>
-#include <okay/core/util/option.hpp>
 #include <nfr_can/CAN_interface.hpp>
 
 class CAN_IMGUI : public ICAN {
-private:
-    struct SignalInfo {
-        char* name;
-        SignalType type;
-        union {
-            int8_t s8;
-            int16_t s16;
-            int32_t s32;
-            int64_t s64;
-            uint8_t u8;
-            uint16_t u16;
-            uint32_t u32;
-            uint64_t u64;
-            float f;
-            bool b;
-        } value;
-    };
-
-    struct MessageChangeInfo {
-        uint32_t messageID;
-        uint8_t signalNum;
-        SignalInfo changedSignal;
-    };
-
-    std::unordered_map<uint32_t, std::vector<struct SignalInfo>> messageIdToSignalsInfo;
 
 public:
     CAN_IMGUI() {}
@@ -40,9 +12,8 @@ public:
     bool init(const BaudRate baud) override;
     bool send(const CAN_Frame& msg) override;
     bool recv(CAN_Frame& msg) override;
-    okay::Option<struct MessageChangeInfo> drawUI();
+    void drawUI();
     uint32_t time_ms() override;
-    SignalInfo* getSignalInfo(uint32_t msgId, uint8_t sigNum);
 };
 
 #endif

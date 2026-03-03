db97e23daa5a4d250c93076499600bb4be82d251
kodeti <kiritortino@gmail.com>
2026-03-02 23:20:07 -0600

    simplify IMGUI indexing to use push/pop IDs over string allocation

--- dash/can/mock/can_imgui.cpp
+++ dash/can/mock/can_imgui.cpp
@@ -87,6 +87,8 @@ okay::Option<CAN_IMGUI::MessageChangeInfo> CAN_IMGUI::drawUI() {
                 uint32_t messageId { message->get_id().id };
 
                 if (ImGui::CollapsingHeader(dbc::meta::messageIdToName.at(messageId), ImGuiTreeNodeFlags_DefaultOpen)) {
+                    ImGui::PushID(messageId);
+
                     for (uint8_t sigNum {}; sigNum < message->get_num_signals(); sigNum++) {
                         ICAN_Signal* signal { message->get_signal(sigNum) };
 
@@ -100,25 +102,29 @@ okay::Option<CAN_IMGUI::MessageChangeInfo> CAN_IMGUI::drawUI() {
                         sigInfo.type = signal->getSignalType();
                         bool changed {};
 
-                        std::string widgetName { std::string(name) + "###" + dbc::meta::messageIdToName.at(messageId) + name };
+                        ImGui::PushID(sigNum);
 
                         switch (signal->getSignalType()) {
-                            case SignalType::INT8:   { auto s = static_cast<CAN_Signal<int8_t>*>(signal);   sigInfo.value.s8  = s->get(); if (ImGui::InputScalar(widgetName.c_str(), ImGuiDataType_S8,  &sigInfo.value.s8))  changed = true; break; }
-                            case SignalType::INT16:  { auto s = static_cast<CAN_Signal<int16_t>*>(signal);  sigInfo.value.s16 = s->get(); if (ImGui::InputScalar(widgetName.c_str(), ImGuiDataType_S16, &sigInfo.value.s16)) changed = true; break; }
-                            case SignalType::INT32:  { auto s = static_cast<CAN_Signal<int32_t>*>(signal);  sigInfo.value.s32 = s->get(); if (ImGui::InputScalar(widgetName.c_str(), ImGuiDataType_S32, &sigInfo.value.s32)) changed = true; break; }
-                            case SignalType::INT64:  { auto s = static_cast<CAN_Signal<int64_t>*>(signal);  sigInfo.value.s64 = s->get(); if (ImGui::InputScalar(widgetName.c_str(), ImGuiDataType_S64, &sigInfo.value.s64)) changed = true; break; }
-                            case SignalType::UINT8:  { auto s = static_cast<CAN_Signal<uint8_t>*>(signal);  sigInfo.value.u8  = s->get(); if (ImGui::InputScalar(widgetName.c_str(), ImGuiDataType_U8,  &sigInfo.value.u8))  changed = true; break; }
-                            case SignalType::UINT16: { auto s = static_cast<CAN_Signal<uint16_t>*>(signal); sigInfo.value.u16 = s->get(); if (ImGui::InputScalar(widgetName.c_str(), ImGuiDataType_U16, &sigInfo.value.u16)) changed = true; break; }
-                            case SignalType::UINT32: { auto s = static_cast<CAN_Signal<uint32_t>*>(signal); sigInfo.value.u32 = s->get(); if (ImGui::InputScalar(widgetName.c_str(), ImGuiDataType_U32, &sigInfo.value.u32)) changed = true; break; }
-                            case SignalType::UINT64: { auto s = static_cast<CAN_Signal<uint64_t>*>(signal); sigInfo.value.u64 = s->get(); if (ImGui::InputScalar(widgetName.c_str(), ImGuiDataType_U64, &sigInfo.value.u64)) changed = true; break; }
-                            case SignalType::FLOAT:  { auto s = static_cast<CAN_Signal<float>*>(signal);    sigInfo.value.f   = s->get(); if (ImGui::InputFloat(widgetName.c_str(),                     &sigInfo.value.f))   changed = true; break; }
-                            case SignalType::BOOL:   { auto s = static_cast<CAN_Signal<bool>*>(signal);     sigInfo.value.b   = s->get(); if (ImGui::Checkbox(widgetName.c_str(),                       &sigInfo.value.b))   changed = true; break; }
+                            case SignalType::INT8:   { auto s = static_cast<CAN_Signal<int8_t>*>(signal);   sigInfo.value.s8  = s->get(); if (ImGui::InputScalar(name, ImGuiDataType_S8,  &sigInfo.value.s8))  changed = true; break; }
+                            case SignalType::INT16:  { auto s = static_cast<CAN_Signal<int16_t>*>(signal);  sigInfo.value.s16 = s->get(); if (ImGui::InputScalar(name, ImGuiDataType_S16, &sigInfo.value.s16)) changed = true; break; }
+                            case SignalType::INT32:  { auto s = static_cast<CAN_Signal<int32_t>*>(signal);  sigInfo.value.s32 = s->get(); if (ImGui::InputScalar(name, ImGuiDataType_S32, &sigInfo.value.s32)) changed = true; break; }
+                            case SignalType::INT64:  { auto s = static_cast<CAN_Signal<int64_t>*>(signal);  sigInfo.value.s64 = s->get(); if (ImGui::InputScalar(name, ImGuiDataType_S64, &sigInfo.value.s64)) changed = true; break; }
+                            case SignalType::UINT8:  { auto s = static_cast<CAN_Signal<uint8_t>*>(signal);  sigInfo.value.u8  = s->get(); if (ImGui::InputScalar(name, ImGuiDataType_U8,  &sigInfo.value.u8))  changed = true; break; }
+                            case SignalType::UINT16: { auto s = static_cast<CAN_Signal<uint16_t>*>(signal); sigInfo.value.u16 = s->get(); if (ImGui::InputScalar(name, ImGuiDataType_U16, &sigInfo.value.u16)) changed = true; break; }
+                            case SignalType::UINT32: { auto s = static_cast<CAN_Signal<uint32_t>*>(signal); sigInfo.value.u32 = s->get(); if (ImGui::InputScalar(name, ImGuiDataType_U32, &sigInfo.value.u32)) changed = true; break; }
+                            case SignalType::UINT64: { auto s = static_cast<CAN_Signal<uint64_t>*>(signal); sigInfo.value.u64 = s->get(); if (ImGui::InputScalar(name, ImGuiDataType_U64, &sigInfo.value.u64)) changed = true; break; }
+                            case SignalType::FLOAT:  { auto s = static_cast<CAN_Signal<float>*>(signal);    sigInfo.value.f   = s->get(); if (ImGui::InputFloat(name,                     &sigInfo.value.f))   changed = true; break; }
+                            case SignalType::BOOL:   { auto s = static_cast<CAN_Signal<bool>*>(signal);     sigInfo.value.b   = s->get(); if (ImGui::Checkbox(name,                       &sigInfo.value.b))   changed = true; break; }
                         }
+
+                        ImGui::PopID();
 
                         if (changed) {
                             result = okay::Option<CAN_IMGUI::MessageChangeInfo>::some({ messageId, sigNum, sigInfo });
                         }
                     }
+
+                    ImGui::PopID();
                 }
             }
         }

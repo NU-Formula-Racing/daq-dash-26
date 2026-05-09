312edf790afd1c75f59bc6210d6e1679b5ed9841
evan-bertis-sample <esample21@gmail.com>
2026-05-09 16:42:45 -0500

    Displaying textures

--- dash/drive_page.hpp
+++ dash/drive_page.hpp
@@ -1,6 +1,7 @@
 #ifndef __DRIVE_PAGE_H__
 #define __DRIVE_PAGE_H__
 
+#include "okay/core/ui/builder.hpp"
 #include "page.hpp"
 
 #include <okay/okay.hpp>
@@ -14,17 +15,29 @@ class DrivePage : public IPage {
     void createEntities() {
         okay::Engine.logger.debug("Creating entities for Drive page!");
 
-        _entities = {okay::ecs::entity()
-                .addComponent<okay::TransformComponent>()
-                .addComponent<okay::UIComponent>([]() {
-                    return ui::image(okay::load::engineTexture("textures/uv_test.jpg"));
-                })};
+        _entities = {okay::ecs::uiEntity(buildBackground, 0),
+            okay::ecs::uiEntity(buildTopHud, 1),
+            okay::ecs::uiEntity(buildBotHud, 1)};
     }
 
     void freeEntities() {
         for (okay::ECSEntity& entity : _entities) {
             entity.destroy();
         }
+    }
+
+    static okay::UIElement buildBackground() {
+        return ui::growbox().backgroundColorSet(glm::vec4{0.0f, 0.0f, 0.0f, 1.0f})(
+            ui::image(okay::load::texture("textures/bg_pattern.png")));
+    }
+
+    static okay::UIElement buildTopHud() {
+        return ui::image(okay::load::texture("textures/top_bar.png"));
+    }
+
+    static okay::UIElement buildBotHud() {
+        return ui::relFrame(0.0f, 0.0f, 1.0f, 1.0f)(
+            ui::spacer(), ui::image(okay::load::texture("textures/bottom_bar.png")));
     }
 
    private:

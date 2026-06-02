8db3457e61d729f524686e2afd9d6b51f54337ec
sophiabian777-collab <sophiabian777@gmail.com>
2026-06-01 21:56:28 -0500

    Fix compilation changes

--- dash/main.cpp
+++ dash/main.cpp
@@ -3,14 +3,11 @@
 #include "ui/components/rotate.hpp"
 #include "ui/debug_page.hpp"
 #include "ui/drive_page.hpp"
-<<<<<<< HEAD
-#include "ui/imu_pdm_tlm_debug_page.hpp"
-#include "ui/inputs.hpp"
-=======
 #include "ui/error_page.hpp"
 #include "ui/inverter_page.hpp"
->>>>>>> inverter-page
+#include "ui/imu_pdm_tlm_debug_page.hpp"
 #include "ui/page.hpp"
+#include "ui/inputs.hpp"
 
 #include <okay/okay.hpp>
 
